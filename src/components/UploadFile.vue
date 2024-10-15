56aaf389a1e689b71afe0aca14f4b6db692d5011
ngovanquy <ngoq753429@gmail.com>
2024-10-15 14:41:39 +0700

    update file

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -2,17 +2,55 @@
   <div>
     <img :src="imageUrl" alt="" />
     <input @change="handleGetFile" type="file" />
-    <button>Upload</button>
+    <button @click="handleUpload">Upload</button>
+    <button @click="handleSubmit">Submit</button>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const imageUrl = ref("");
+import {
+  getDownloadURL,
+  ref as storageRef,
+  uploadBytes,
+} from "firebase/storage";
+import { storage } from "@/firebase";
 
+const imageUrl = ref("");
+const file = ref(null);
+const downloadUrl = ref("");
+
+// Lấy giá trị trong ô input type file và privew hình ảnh
 const handleGetFile = (e) => {
-  console.log("Event: ", e.target.files[0]);
+  imageUrl.value = URL.createObjectURL(e.target.files[0]);
+
+  file.value = e.target.files[0];
+};
+
+// Lưu hình ảnh lên firebase storage
+const handleUpload = async () => {
+  if (!file) {
+    alert("Chưa có file được chọn");
+  } else {
+    try {
+      // Tạo tham chiếu đến folder được lưu trữ trên firebase
+      const storageReference = storageRef(storage, `images/${file.value.name}`);
+
+      // Lưu hình ảnh trên firebase
+      await uploadBytes(storageReference, file.value);
+
+      // Lấy url sau khi đã upload xong
+
+      downloadUrl.value = await getDownloadURL(storageReference);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+const handleSubmit = () => {
+  console.log("Image url: ", downloadUrl.value);
 };
 </script>
 
